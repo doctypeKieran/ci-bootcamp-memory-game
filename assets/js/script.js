1b98c349726f27c1cd4484b2f98b354e208740aa
Kieran Hillman <developedbykieran@gmail.com>
2023-10-19 08:46:15 +0000

    Create colors array, shuffle them, create squares with random colours dynamically

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,30 @@
-console.log("YES");
+// Create array to hold colors
+let colors = ["red", "red", "green", "green", "blue", "blue", "yellow", "yellow", "orange", "orange", "purple", "purple"];
+
+// Create a function to shuffle arrays
+function shuffleArray(arr) {
+    let currentIndex = arr.length;
+    let randomIndex;
+
+    while(currentIndex > 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+
+        [arr[currentIndex], arr[randomIndex]] = [arr[randomIndex], arr[currentIndex]];
+    }
+    return arr;
+}
+
+// And shuffle the colors array:
+shuffleArray(colors);
+
+// Variable for the gameboard
+const gameBoard = document.getElementById('gameBoard');
+
+for (let color of colors) {
+    const card = document.createElement('div');
+    card.classList.add('card');
+    card.style.backgroundColor = color;
+    // ADD HANDLECLICK HERE
+    gameBoard.appendChild(card);
+}
